3e794d38f7bb6812340030d1395fdbbc589beded
Dungo7k <zagoorski.l@gmail.com>
2017-11-30 22:08:26 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cs-cxpvz7jca*n^r4^#3#7e9$!ebliv@j+$!d(v@(#jfw5e+sr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'Dungo.pythonanywhere.com']
 
